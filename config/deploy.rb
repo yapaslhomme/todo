c786d43878a820bb0ac0f312fe755c9f0411059b
yapaslhomme <formensah@gmail.com>
2014-10-18 18:35:10 +0200

    debut

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,10 +10,11 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'foobar.com'
+set :user, 'deploy'
+set :domain, 'ec2-54-171-9-154.eu-west-1.compute.amazonaws.com'
 set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
-set :branch, 'master'
+#set :repository, 'git://...'
+#set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
@@ -53,14 +54,16 @@ task :deploy => :environment do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
-    invoke :'git:clone'
+    #invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
 
     to :launch do
-      queue "touch #{deploy_to}/tmp/restart.txt"
+      queue "bundle exec unicorn -c config/unicorn/production.rb -E production - D"
+
+      #queue "touch #{deploy_to}/tmp/restart.txt"
     end
   end
 end
