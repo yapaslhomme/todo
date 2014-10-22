e0931d994b9bea1dd89ae958c1bbe7b047ec5684
yapaslhomme <formensah@gmail.com>
2014-10-22 09:02:27 +0200

    datae

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :repo_url, 'git@github.com:yapaslhomme/todo.git'
  set :pty, true
 
 # Default value for :linked_files is []
- #set :linked_files, %w{config/database.yml}
+ set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
  #set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -32,7 +32,7 @@ set :repo_url, 'git@github.com:yapaslhomme/todo.git'
  set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+ set :keep_releases, 5
 
 namespace :deploy do
 
